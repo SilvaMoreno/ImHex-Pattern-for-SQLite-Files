a6a8a3fab7246d408fd1e457ee2708f80c973a18
Elton Moreno <emoreno@qualimaking.com>
2023-06-20 09:59:36 +0100

    sql info check

---
+++ info_check.sql
@@ -0,0 +1,23 @@
+PRAGMA page_count;
+
+PRAGMA page_size;
+
+PRAGMA max_page_count;
+
+PRAGMA cache_size;
+
+PRAGMA freelist_count;
+
+pragma default_cache_size;
+
+pragma auto_vacuum;
+
+PRAGMA incremental_vacuum;
+
+PRAGMA stats;
+
+PRAGMA table_list;
+
+PRAGMA user_version;
+
+PRAGMA encoding;
